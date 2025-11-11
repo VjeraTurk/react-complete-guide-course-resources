1f88f95a7fa2321fbf2a3585c2f0909439b98a74
Vjera Turk <vjera.turk@gmail.com>
2025-11-11 22:12:52 +0100

    Edit Player names

---
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,35 @@
+import {useState } from 'react';
+
+export default function Player({initialName, symbol}){
+
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] =  useState(false);
+
+    function handleEditClick() {
+        // DO NOT change state like this:
+        // setIsEditing(!isEditing)
+
+        // instead always PASS A FUNCTION
+        // setIsEditing((editing)=>{!editing});
+        setIsEditing((editing)=>!editing);
+
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    return (
+        <li>
+            <span className="player">
+                { isEditing ?
+                    <input type="text" required value={playerName} onChange={handleChange}></input>:
+                    <span className="player-name">{playerName}</span>
+                }
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing? 'Save' : 'Edit'}</button>
+      </li>
+
+    );
+}

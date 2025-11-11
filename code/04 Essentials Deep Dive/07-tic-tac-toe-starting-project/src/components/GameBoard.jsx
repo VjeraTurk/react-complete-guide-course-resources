500235e3c7aa0877b1b2b51cde21e78ac6ec5fcd
Vjera Turk <vjera.turk@gmail.com>
2025-11-11 23:24:32 +0100

    Add X on click

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,4 +1,5 @@
 
+import { useState } from "react"
 const initialGameBoard = [
 [null, null, null],
 [null, null, null],
@@ -7,13 +8,33 @@ const initialGameBoard = [
 
 export default function GameBoard() {
 
+    const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+
+    function handleSelectSquare(rowIndex, colIndex){
+        setGameBoard((prevGameBoard)=>{
+            /**
+             * it's also strongly recommended that if your state
+             * is an object or array, you update that state
+             * in an immutable way, which simply means you create a copy
+             * of the old state, so a new object or a new array first,
+             * and you then just change that copy instead
+             * of that existing object or array.
+             */
+            const updatedBoard =  [...prevGameBoard.map(innerArray => [...innerArray])]
+            updatedBoard[rowIndex][colIndex] = 'X';
+            // console.log(updatedBoard[rowIndex][colIndex])
+            return updatedBoard;
+        })
+    }
+
     return <ol id="game-board">
         {
-            initialGameBoard.map(( row, rowIndex)=><li key={rowIndex}>
+            gameBoard.map(( row, rowIndex)=><li key={rowIndex}>
                 <ol>
                     { row.map( (playerSymbol, colIndex) =>
                     <li key={colIndex}>
-                        <button>{playerSymbol}</button>
+                        <button onClick={() => handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
                     </li>)}
                 </ol>
             </li>)
